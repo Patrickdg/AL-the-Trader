c7ba91fd43757d7b2e097ad19dd3c545721efe50
Patrickdg <deguzman.patrick@outlook.com>
2019-12-22 06:16:14 -0500

    initialized testing

--- al-the-trader.py
+++ al-the-trader.py
@@ -1,17 +1,33 @@
 #==============================
 # Resources
 
+## Finviz
+### https://github.com/mariostoev/finviz
 
+## yFinance
+### https://pypi.org/project/yfinance/
 
 
 
 #==============================
 # Libraries
-
 import finviz
+from finviz.screener import Screener
+from finviz.portfolio import Portfolio
+import pandas as pd
 
-
-
-
+import yfinance as yf
 
 #==============================
+
+# Individual Stocks
+aapl = finviz.get_stock('AAPL')
+aapl.keys()
+
+
+
+hist = yf.Ticker('MSFT')
+hist.history(period = "max")
+hist.calendar
+
+
