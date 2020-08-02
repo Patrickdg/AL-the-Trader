6961619aa2fa45ba82466f72b42127cfa2fffccf
Patrickdg <patrickdeguzman20@hotmail.com>
2020-08-02 15:27:41 -0400

    complete data compilation, v1

--- ml/data_compilation_stock.py
+++ ml/data_compilation_stock.py
@@ -1,60 +1,61 @@
 '''
-For each stock in watchlist, gather stock price + volume data
+For each stock in watchlist, gather stock price + volume data + rolling aggregates
 '''
 
 #LIBRARIES
 import pandas as pd
 import numpy as np
 import yfinance as yf
+import ml.rolling_agg_funcs as ra
 
-#DECLARATIONS
+import imp
+imp.reload(ra)
+
+#DECLARATIONS/PARAMS
 watchlist = pd.read_excel('ml/watchlist_ml.xlsx', sheet_name = 'watchlist')
-
-period = '5y'
-
-#FUNCS
-def rolling_col_mean(df, col_name, period):
-    ma_col = df[col_name].rolling(period).mean()
-    return ma_col
-def rolling_col_max(df, col_name, period):
-    ma_col = df[col_name].rolling(period).max()
-    return ma_col
-def rolling_col_min(df, col_name, period):
-    ma_col = df[col_name].rolling(period).min()
-    return ma_col
+period = '5y' # for individual stock history
+cols = ['Open','High','Low','Close','Volume']
+drop_cols = ['Dividends', 'Stock Splits']
+periods = [5, 10, 15, 21, 130, 260] # for rolling aggregate calcs
+funcs = [ra.rolling_mean, ra.rolling_max, ra.rolling_min, ra.rolling_stdev, ra.z_score]
+benchmark_ticker = 'VTSMX' # The Vanguard Total Stock Market Index 
 
 #SET BENCHMARK INDEX
-benchmark_ticker = 'VTSMX' # The Vanguard Total Stock Market Index 
 benchmark_history = yf.Ticker(benchmark_ticker).history(period = period)
+benchmark_history = ra.add_rolling_cols(benchmark_history, cols, periods, funcs).drop(drop_cols, axis = 1)
+benchmark_history.columns = [f'market_{col}' for col in benchmark_history.columns]
 
 # MAIN LOOP
 assets = pd.DataFrame() 
 for ticker in watchlist.ticker: 
-    # Initialize asset info
+    # Initialize asset history
     asset = yf.Ticker(ticker)
     asset_figs = asset.history(period = period)
-    
-    # Add Moving average columns
-    for col in ['Open','High','Low','Close','Volume']:
-        for n in [7, 14, 21 ,28]: 
-            for func in [rolling_col_mean, rolling_col_max, rolling_col_min]:
-                function = func.__name__.replace("rolling_col_", "")
-                asset_figs[f'{col}_ma_{function}_{n}'] = func(asset_figs, col, n)
+    # Add Rolling columns
+    asset_figs = ra.add_rolling_cols(asset_figs, cols, periods, funcs).drop(drop_cols, axis = 1)
 
     # JOIN: Benchmark info
+    asset_figs = pd.merge(asset_figs, benchmark_history, on = 'Date')
 
-    # Calculate growth rates
-    asset_figs = asset_figs.diff()/asset_figs.shift(1)
+    # Calculate growth rates, concat with original figs and ticker info
+    deltas = asset_figs.diff()/asset_figs.shift(1)
+    deltas.columns = [f'{col}_delta' for col in deltas.columns]
 
-    # add ticker
+    asset_figs = pd.concat([asset_figs[cols],
+                            asset_figs.iloc[:, ['z_score' in col for col in asset_figs.columns]],
+                            deltas], 
+                            axis = 1)
     asset_figs['sector'] = asset.info['sector']
 
-
-    print(ticker)
     assets = pd.concat([assets, asset_figs]); print(assets.shape)
-    
-# Reorder columns
+    print(ticker)
+    if ticker == 'WMT': 
+        break
+# Drop all Nan columns, Reorder columns
+assets.dropna(how = 'all', axis = 1, inplace = True); assets.dropna(how = 'any', axis = 0, inplace = True)
 assets.reset_index(inplace = True)
+
 first_cols = ['Date','sector']; rem_cols = [col for col in assets.columns if col not in first_cols]
-assets = assets[first_cols+rem_cols].reset_index(drop = True)
-# assets.to_excel('ml/consolidated_asset_stock.xlsx')
+assets = assets[first_cols+rem_cols]
+
+assets.to_excel('ml/consolidated_asset_stock.xlsx')
---
+++ ml/rolling_agg_funcs.py
@@ -0,0 +1,25 @@
+def rolling_mean(df, col_name, period):
+    rolling = df[col_name].rolling(period).mean()
+    return rolling
+def rolling_max(df, col_name, period):
+    rolling = df[col_name].rolling(period).max()
+    return rolling
+def rolling_min(df, col_name, period):
+    rolling = df[col_name].rolling(period).min()
+    return rolling
+def rolling_stdev(df, col_name, period):
+    rolling = df[col_name].rolling(period).std()
+    return rolling
+def z_score(df, col_name, period):
+    x_bar = rolling_mean(df, col_name, period)
+    stdev = rolling_stdev(df, col_name, period)
+    rolling = (df[col_name] - x_bar)/stdev
+    return rolling
+
+def add_rolling_cols(df, cols, periods, funcs): 
+    for col in cols:
+        for n in periods: 
+            for func in funcs:
+                agg_name = func.__name__
+                df[f'{col}_{agg_name}_{n}'] = func(df, col, n)
+    return df
